4c4343108b586c5238858050be3ddfd15b2b025b
Julien Greffe <jgreffe@talend.com>
2019-11-19 10:20:41 +0100

    TESB-27385: Runtime "org.ops4j.pax.logging.cfg" overwritten by Cloud-RemoteEngine
    Adapted cherry-pick of KARAF-6519

--- config/src/main/java/org/apache/karaf/config/core/ConfigMBean.java
+++ config/src/main/java/org/apache/karaf/config/core/ConfigMBean.java
@@ -121,6 +121,24 @@ public interface ConfigMBean {
      */
     void update(String pid, Map<String, String> properties) throws MBeanException;
 
+    /**
+     * Add new properties or update existing ones (without removing others) in a given configuration.
+     *
+     * @param pid the configuration PID.
+     * @param properties the properties to add/update.
+     * @throws MBeanException in case of MBean failure.
+     */
+    void append(String pid, Map<String, String> properties) throws MBeanException;
+
+    /**
+     * Delete properties from a configuration.
+     *
+     * @param pid the configuration PID.
+     * @param properties the properties to delete from the configuration.
+     * @throws MBeanException in case of MBean failure.
+     */
+    void delete(String pid, List<String> properties) throws MBeanException;
+
     String createFactoryConfiguration(String factoryPid) throws MBeanException;
 
     String createFactoryConfiguration(String factoryPid, String alias) throws MBeanException;
